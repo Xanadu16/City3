a730f452bd8712367f006c0e3f3c594c5a858b8b
Evar678 <Bxcar92@gmail.com>
2013-03-24 00:24:17 -0400

    Added commands.cs and packages.cs

--- server.cs
+++ server.cs
@@ -1,7 +1,8 @@
-//City3 Server.cs
+yes//City3 Server.cs
 //Version: 0.0.0.0
 //Last edit: 3/23/13
 
 //Load all necessary scripts
 exec("Gamemody_City3/ScriptObjects.cs");
-//derp
+exec("Gamemode_City3/Commands.cs");
+exec("Gamemode_City3/Packages.cs");

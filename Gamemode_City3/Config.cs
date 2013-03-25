b60798e7e13bc85090f9d494dd398278ad55d12c
Evar678 <Bxcar92@gmail.com>
2013-03-25 14:15:45 -0400

    Deleted Gamemode_City3 folder
    Replaced by the City3 folder.

--- Gamemode_City3/Config.cs
+++
@@ -1,9 +0,0 @@
-//  ____ _ _           _____
-// / ___(_) |_ _   _  |___ / ®
-//| |   | | __| | | |   |_ \
-//| |___| | |_| |_| |  ___) |
-// \____|_|\__|\__, | |____/
-//             |___/
-
-$City3::UserSaves = "Config/Server/City3/Users/";
-$City3::SaveTime = 10; //in minutes (will be used later)
